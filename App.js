690de8a2df157d12dc6a0b01bb37be5a4979a1a5
alestor123 <alestoraldous@gmail.com>
2021-01-29 20:44:56 +0530

    added main code

---
+++ App.js
@@ -0,0 +1,11 @@
+const { dir } = require('console');
+
+var fs = require('fs'),
+path = require('path'),
+read = require('util').promisify(fs.readdir),
+dirss = [];
+module.exports = async pathname => {
+var data = await read(pathname);
+data.map(dirs => {if(fs.statSync(dirs).isDirectory()) dirss.push(dirs)})
+return dirss
+}
